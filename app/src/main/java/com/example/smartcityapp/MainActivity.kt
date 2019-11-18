54356038a68ae20647623a6bb4d4218003252f21
michelle_sajorda <sajorda.mich@gmail.com>
2019-11-18 16:39:53 +0800

    sample project commit

---
+++ app/src/main/java/com/example/smartcityapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.smartcityapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+public class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
